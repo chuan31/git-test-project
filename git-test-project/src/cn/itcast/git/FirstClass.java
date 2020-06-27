982af4e7be2e5f84716bd5c3046fc8ba576dc917
Lee <lichuan.mr@foxmail.com>
2020-06-27 19:34:06 +0800

    把本地工程添加到git

---
+++ git-test-project/src/cn/itcast/git/FirstClass.java
@@ -0,0 +1,17 @@
+package cn.itcast.git;
+
+/**
+ * Created by Lee on 2020/6/27.
+ */
+public class FirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+
+}
